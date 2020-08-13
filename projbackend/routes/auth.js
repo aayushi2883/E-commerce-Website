0d28913a36bb214043d111eeeb4850ef05ca7f54
Aayushi <aatulasyan143@gmail.com>
2020-08-13 11:50:54 +0530

    modified

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,8 +1,13 @@
 const express = require("express");
 var router = express.Router();
+const {check, validationResult } = require("express-validator");
 const {signout, signup} = require("../controllers/auth")
 
-router.post("/signup" , signup);
+router.post("/signup" , [
+    check("name" , "name should be of atleast 3 characters").isLength({ min: 3}),
+    check("email" , "email is a required field").isEmail(),
+    check("password" , "password should be atleast of 5 characters").isLength({ min:5 })
+] ,  signup);
 router.get("/signout" ,signout );
 
 module.exports = router;
